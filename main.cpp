5c30ed0245729a70115570ddc978f4a0a3e87e9c
BuildTools <unconfigured@null.spigotmc.org>
2020-11-19 22:25:58 +0100

    whatsurname

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string name;
+    cout << "Wie lautet dein Name: ";
+
+    cin >> name;
+
+    cout << "Hallo " << name << " wat geht";
+}
